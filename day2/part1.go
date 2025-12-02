69577fc48e281e16d494d2af91937d28cd965c0a
Antonio Fasanella <fasanella.antonio.sanfele@gmail.com>
2025-12-02 10:52:53 +0100

    day 2 part 1 solution

---
+++ day2/part1.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func part1() {
+	fmt.Println("Day 2, Part 1")
+	data, err := os.ReadFile("day2/input.txt")
+	if err != nil {
+		fmt.Println("Error reading input file:", err)
+		return
+	}
+
+	ranges := strings.Split(string(data), ",")
+
+	sum := 0
+
+	for _, r := range ranges {
+		fmt.Println("Processing range:", r)
+		ids := strings.Split(r, "-")
+		firstId, err := strconv.Atoi(ids[0])
+		if err != nil {
+			fmt.Println("Error converting first ID:", err)
+			continue
+		}
+		lastId, err := strconv.Atoi(ids[1])
+		if err != nil {
+			fmt.Println("Error converting last ID:", err)
+			continue
+		}
+		for id := firstId; id <= lastId; id++ {
+			//fmt.Println("Processing ID:", id)
+			sId := strconv.Itoa(id)
+			if len(sId)%2 != 0 {
+				continue
+			}
+			found := true
+			for i := 0; i < len(sId)-len(sId)/2; i++ {
+				digit1, _ := strconv.Atoi(string(sId[i]))
+				digit2, _ := strconv.Atoi(string(sId[(len(sId)/2)+i]))
+				if digit1 != digit2 {
+					found = false
+					break
+				}
+			}
+			if found {
+				fmt.Println("Invalid id: ", id)
+				sum += id
+			}
+
+		}
+
+		fmt.Println("Current sum:", sum)
+
+	}
+
+}
